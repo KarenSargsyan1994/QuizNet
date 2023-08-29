05a34599d407032986d860414cdb81841c93eafa
Hamlet Poghosyan <hamlet.poghosyan@volo.global>
2023-08-29 10:36:06 +0400

    add codes

---
+++ QuizNet/Infrastructure/Repositories/ShipmentRepository.cs
@@ -0,0 +1,86 @@
+﻿using Domain;
+using Domain.Enums;
+using Domain.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Repositories
+{
+    public class ShipmentRepository : IShipmentRepository
+    {
+        private readonly AppDbContext _context;
+
+        public ShipmentRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public Task<Shipment> FindByIdAsync(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Shipment> FindByIdAsync_Correct(long shipmentId, params ShipmentModules[] includes)
+        {
+            if (shipmentId == 0)
+                return null;
+
+            var query = _context.Shipments.AsQueryable();
+
+            foreach (ShipmentModules include in includes)
+            {
+                switch (include)
+                {
+                    case ShipmentModules.Containers:
+                        query = query.Include(s => s.Containers);
+                        break;
+                    case ShipmentModules.Items:
+                        query = query.Include(s => s.Items);
+                        break;         
+                    case ShipmentModules.Transports:
+                        query = query.Include(s => s.Transports);
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            return await query.FirstOrDefaultAsync(s => s.Id == shipmentId);
+        }
+
+        public async Task<Shipment> FindByIdAsync_Containers(long id )
+        {
+            if (id == 0)
+                return null;
+
+            var query = _context.Shipments
+                .Include(x => x.Containers)
+                .AsQueryable();
+
+            return await query.FirstOrDefaultAsync(s => s.Id == id);
+        }
+
+        public async Task<Shipment> FindByIdAsync_Transports(long id)
+        {
+            if (id == 0)
+                return null;
+
+            var query = _context.Shipments
+                .Include(x => x.Transports)
+                .AsQueryable();
+
+            return await query.FirstOrDefaultAsync(s => s.Id == id);
+        }
+
+        public async Task<Shipment> FindByIdAsync_Items(long id)
+        {
+            if (id == 0)
+                return null;
+
+            var query = _context.Shipments
+                .Include(x => x.Items)
+                .AsQueryable();
+
+            return await query.FirstOrDefaultAsync(s => s.Id == id);
+        }
+    }
+}
---
+++ QuizNet/QuizNet/Controllers/ShipmentController.cs
@@ -0,0 +1,70 @@
+﻿using Domain.Enums;
+using Domain.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace QuizNet.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ShipmentController : ControllerBase
+    {
+        private readonly IShipmentRepository _shipmentRepository;
+        public ShipmentController(IShipmentRepository shipmentRepository)
+        {
+            _shipmentRepository = shipmentRepository;
+        }
+
+        #region Incorrect Functions for Find Shipment By Id
+
+        /// <summary>
+        /// The current implementation includes three endpoints that essentially perform the same operation but with different sets of included related entities.
+        /// This results in code duplication, where each new inclusion requires the addition of a new endpoint or a new function.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        /// 
+
+        [HttpGet("FindByIdWithContainers")]
+        public async Task<IActionResult> FindById_Containers(int id)
+        {
+            var result = await _shipmentRepository.FindByIdAsync_Containers(id);
+            return Ok(result);
+        }
+
+        [HttpGet("FindByIdWithTransports")]
+        public async Task<IActionResult> FindById_Transports(int id)
+        {
+            var result = await _shipmentRepository.FindByIdAsync_Transports(id);
+            return Ok(result);
+        }
+
+        [HttpGet("FindByIdWithItems")]
+        public async Task<IActionResult> FindById_Items(int id)
+        {
+            var result = await _shipmentRepository.FindByIdAsync_Items(id);
+            return Ok(result);
+        }
+
+        #endregion
+
+        #region     Correct Function for Find Shipment By Id
+
+        /// <summary>
+        /// This function support dynamic includes,
+        ///  By doing so, you can provide a single endpoint that accepts the desired related entities as parameters, eliminating the need for duplicating code.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="shipmentModules"></param>
+        /// <returns></returns>
+        [HttpPost("FindByIdCorrect")]
+        public async Task<IActionResult> FindById(int id, [FromBody] ShipmentModules[] shipmentModules)
+        {
+            var result = await _shipmentRepository.FindByIdAsync_Correct(id, shipmentModules);
+            return Ok(result);
+        }
+
+        #endregion
+
+    }
+}
